b8860ad5bac5f13ee7800b2c14ca5277ff15f5fe
SAURAV <SAURAV@192.168.0.3>
2026-09-13 21:41:50 +0530

    next final commit

--- CollectService/src/main/java/com/infinity/constants/CollectConstants.java
+++ CollectService/src/main/java/com/infinity/constants/CollectConstants.java
@@ -8,9 +8,9 @@ public class CollectConstants {
 
 	public static final String PG_MERCHANT_ID = "YBL000000097501";
 
-	public static final String MERCHANT_KEY = System.getenv("MERCHANT_KEY");
+//	public static final String MERCHANT_KEY = System.getenv("MERCHANT_KEY");
 	
-	
+	public static final String MERCHANT_KEY = "50c6cf18cfd0a7d2dbac5cc69d82280dad2356ff1d92a4a1c69ef49f3032571d";
 	
 }
 
